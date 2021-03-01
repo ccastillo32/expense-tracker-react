1b80e026e68a7f10c12a77929de169358e83438c
ccastillo33 <cristian.castillo@tecso.com.co>
2021-03-01 15:24:42 -0500

    Small changes

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,13 +5,13 @@ import { displayNumber } from "../utils/displayNumber";
 
 const Balance = () => {
 
-    const context = useContext(GlobalContext);
-    const { transactions } = context;
+    const { transactions } = useContext(GlobalContext);
+    
     const balance = calculateBalance( transactions );
 
     return (
         <div>
-            <h4>YOUR BALANCE</h4>
+            <h4>YOUR BALANCE:</h4>
             <h1>{ displayNumber(balance) }</h1>
         </div>
     );
