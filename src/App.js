4acbdf56751ae99b517709e7bffa0996aa20856f
ccastillo33 <cristian.castillo@tecso.com.co>
2021-03-01 13:57:15 -0500

    Balance component (style)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Balance from "./components/Balance";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      
       <Header />
+      
+      <div className="container">
+        <Balance />
+      </div>
+
     </div>
   );
 }
