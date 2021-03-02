f1a2b37722a5919248c542573be7b2da5f65c6a3
ccastillo33 <cristian.castillo@tecso.com.co>
2021-03-02 12:35:47 -0500

    Show transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,26 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+import { displayNumber } from "../utils/displayNumber";
+
 const TransactionList = () => {
+
+    const { transactions } = useContext(GlobalContext);
 
     return (
         <div>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-                    Expense
-                    <span>-$400</span>
-                    <button className="delete-btn">X</button>
-                </li>
+
+                {
+                    transactions.map(tx => (
+                        <li className={ tx.type === 'expense' ? 'minus' : 'plus' }>
+                            { tx.text }
+                            <span>{ displayNumber( tx.type === 'expense' ? tx.amount * -1 : tx.amount ) }</span>
+                            <button className="delete-btn">X</button>
+                        </li>
+                    ))
+                }
+
             </ul>
         </div>
     );
